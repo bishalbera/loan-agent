e4ba771973a50b7eb0c135e954f4f988d5501528
Bishal <bishalbera2@gmail.com>
2023-12-15 13:05:30 +0530

    initial commit

---
+++ backend/db/connect.py
@@ -0,0 +1,17 @@
+from backend.env import config
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = config("DATABASE_URL", cast=str, default=None)
+DATABASE_URL is not None
+
+engine = create_engine(str(DATABASE_URL))
+SessionLocal = sessionmaker(bind=engine)
+
+
+def get_db_session():
+    session = SessionLocal()
+    try:
+        yield session
+    finally:
+        session.close()
